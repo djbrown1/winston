7e6e782abb67c75f09860f42d640ca6f3097639a
David Brown <david.j.brown.28@gmail.com>
2019-06-25 12:08:42 -0400

    Initialized sentence analysis script

---
+++ sentence_analysis/attempt1.py
@@ -0,0 +1,9 @@
+"""
+First attempt at using machine learning to analyze sentences to break apart a
+sentence into it's base parts.
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
